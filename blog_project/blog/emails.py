f4764f62c4b713d18d435210f53255d631f18adb
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-04-18 12:05:22 +0600

    Email

--- blog_project/blog/emails.py
+++ blog_project/blog/emails.py
@@ -1,3 +1,21 @@
+# # blog_project/blog/emails.py
+# from django.core.mail import send_mail
+# import random
+# from django.conf import settings
+# from .models import CustomUser
+
+# def send_otp_via_email(email):
+#     otp = random.randint(100000, 999999)
+#     subject = f'Your OTP for Email Verification'
+#     message = f'Your OTP for Email Verification is {otp}'
+#     email_from = settings.EMAIL_HOST_USER
+#     print(f"Sending OTP {otp} to {email}")  
+#     send_mail(subject, message, email_from, [email])
+#     user_obj = CustomUser.objects.get(email=email)
+#     user_obj.otp = otp
+#     user_obj.save()
+
+
 # blog_project/blog/emails.py
 from django.core.mail import send_mail
 import random
@@ -14,3 +32,14 @@ def send_otp_via_email(email):
     user_obj = CustomUser.objects.get(email=email)
     user_obj.otp = otp
     user_obj.save()
+
+def send_notification_email(user_email, subject, message):
+    """Send notification emails to users"""
+    email_from = settings.EMAIL_HOST_USER
+    send_mail(
+        subject,
+        message,
+        email_from,
+        [user_email]
+    )
+    return True
